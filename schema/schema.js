0a013e07e1b5371ed64158a1a27acc3a4b94f376
abeTavarez <abeincwireless@gmail.com>
2021-10-09 14:39:50 -0400

    root query

--- schema/schema.js
+++ schema/schema.js
@@ -1,10 +1,19 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const {
     GraphQLObjectType,
     GraphQLInt,
     GraphQLString
 } = graphql;
+
+// dummy data
+const users = [
+    {id: 3, firstName: 'Abe', age: 28},
+    {id: 11, firstName: 'Eren', age: 18}
+]
+
+
 
 // This object tell graphql of how an user object 
 // looks like or what properties it's supposed to have
@@ -14,5 +23,22 @@ const UserType = new GraphQLObjectType({
         id: {type: GraphQLString},
         firstName: {type: GraphQLString},
         age: {type: graphql.GraphQLInt}
+    }
+});
+
+
+// The RootQuery is how we provide graphql to enter our application data graph
+// args is use to specify which argument we'll use in our query
+// The resolve function is use to specify what piece of data will be return from our database
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLString}},
+            resolve(parentValue, args) {
+                return _.find(users, {id: args.id})
+            }
+        }
     }
 })
